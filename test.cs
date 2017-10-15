0f4bceca751186bc9ef158c1ab4bf3d1ce7042a2
MiguelJack <miguelfenix16@gmail.com>
2017-10-14 19:23:35 -0600

    Resta
    Caso resta simple

--- test.cs
+++ test.cs
@@ -17,5 +17,14 @@ namespace calculadora
             Calc calculadora = new Calc();
             Assert.AreEqual(15, calculadora.Suma(10, 5));
         }
+
+        [TestCase]
+        //Prueba de resta en caso de que los numeros sean simples
+        //no se encuantrar en los limites, ni sean de punto decimal
+        public void RestaNumeroNormal()
+        {
+            Calc calculadora = new Calc();
+            Assert.AreEqual(20, calculadora.Resta(28, 8));
+        }
     }
 }
