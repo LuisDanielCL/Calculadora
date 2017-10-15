ac40866849afb1c24b63fba288172f32eb70f5f8
LuisDanielCL <daniel.cordero.leonhardes@gmail.com>
2017-10-14 18:34:51 -0600

    cambio de nombre a metodo sum

--- calc.cs
+++ calc.cs
@@ -6,11 +6,51 @@ using System.Threading.Tasks;
 
 namespace calculadora
 {
+
+
     class Calc
     {
-        void sum() {
+        private float memoria = 0;
 
+        float Suma(float primerNumero,float segundoNumero) {
+            float salida = primerNumero + segundoNumero;
+            SalvarMemoria(salida);
+            return memoria;
+        }
 
+        float Resta(float primerNumero, float segundoNumero)
+        {
+            float salida = primerNumero - segundoNumero;
+            SalvarMemoria(salida);
+            return memoria;
+        }
+
+        float Multiplica(float primerNumero, float segundoNumero)
+        {
+            float salida = primerNumero * segundoNumero;
+            SalvarMemoria(salida);
+            return memoria;
+        }
+
+        float Divide(float primerNumero, float segundoNumero)
+        {
+            float salida = primerNumero / segundoNumero;
+            SalvarMemoria(salida);
+            return memoria;
+        }
+
+        void BorrarMemoria()
+        {
+            memoria = 0;
+        }
+
+        void SalvarMemoria(float numero) {
+            memoria = numero;
+        }
+
+        float GetMemoria()
+        {
+            return memoria;
         }
     }
 }
