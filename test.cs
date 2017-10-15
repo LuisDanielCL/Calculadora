7f0908d92d59e62745777855386541e2a66a65fd
LuisDanielCL <daniel.cordero.leonhardes@gmail.com>
2017-10-14 18:30:00 -0600

    Se creo la clase calc y metodos
    La clase calc permite realizar las funciones basicas de una calculadora, ademas de que incluye

---
+++ test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    [TestFixture]
+    class Test
+    {
+        [TestCase]
+        public void add() {
+            Calc calculadora = new Calc();
+
+        }
+    }
+}
